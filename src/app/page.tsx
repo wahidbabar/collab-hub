bdb947ce086997a80c98541a6d88515531db5fc4
Abdul Wahid <bbabar0214@gmail.com>
2024-10-06 15:04:00 +0500

    added user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
-import { useRouter } from "next/navigation";
+import UserButton from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-  const router = useRouter();
-
-  const handleSignOut = () => {
-    signOut().then(() => {
-      router.push("/");
-    });
-  };
-
   return (
     <div>
-      Logged in <Button onClick={handleSignOut}>Sign out</Button>
+      <UserButton />
     </div>
   );
 }
---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import React from "react";
+import { useCurrentUser } from "../hooks/use-current-user";
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { useRouter } from "next/navigation";
+
+const UserButton = () => {
+  const router = useRouter();
+  const { data: user, isLoading } = useCurrentUser();
+  const { signOut } = useAuthActions();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  const { name, image } = user;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
+  const handleSignOut = () => {
+    signOut().then(() => {
+      router.push("/");
+    });
+  };
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-sky-500 text-white">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem
+          onClick={handleSignOut}
+          className="h-10 cursor-pointer"
+        >
+          <LogOut className="size-4 mr-2" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserButton;
