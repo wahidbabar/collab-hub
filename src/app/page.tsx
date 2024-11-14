b2279f4e95eccfaa7073c6d891521b91eab12872
wahidbabar <bbabar0214@gmail.com>
2024-11-15 01:09:47 +0500

    fixed loader homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
   }, [workspaceId, isLoading, router]);
 
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      <Loader className="size-5 animate-spin" />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-5 animate-spin text-muted-foreground" />
     </div>
   );
 }
